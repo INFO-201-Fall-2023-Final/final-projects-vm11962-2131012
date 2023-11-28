dd58fa50006dc2fb05ea8c47c137cb3a8afd4999
vm11962-2131012 <92124097+vm11962-2131012@users.noreply.github.com>
2023-11-27 23:34:43 -0800

    Update DataWrangling.R

--- DataWrangling.R
+++ DataWrangling.R
@@ -2,12 +2,10 @@ library(dplyr)
 library(stringr)
 
 annapurna_df <- read.csv("annapurna.csv") 
-#accidents_df <- read.csv("accidents_git.csv") 
 mount_e_df <- read.csv("mount_everest_deaths.csv")
-#members_df <- read.csv("members.csv") 
 
-#You fist need to to create a unified dataset. This means that the records in your 
-#two datasets need to be related some how, either by a shared key or a combination of fields.
+#You first need to create a unified dataset. This means that the records in your 
+#two datasets need to be related somehow, either by a shared key or a combination of fields.
 annapurna_df$Year <- substr(annapurna_df$Date, nchar(annapurna_df$Date) -3, nchar(annapurna_df$Date))  
 mount_e_df$Year <- substr(mount_e_df$Date, nchar(mount_e_df$Date) -4, nchar(mount_e_df$Date))  
 
@@ -20,16 +18,17 @@ for (col in col_to_con) {
   new_df[[col]] <- paste(new_df[[paste0(col, ".x")]], new_df[[paste0(col, ".y")]], sep = " ")
 }
 
-
 col_to_rem <- c("Date", "Name.x", "Name.y", "Year.x", "Year.y", "Nationality.x", "Nationality.y", "Cause.of.death.x", "Cause.of.death.y")
 new_df <- new_df[, -which(names(new_df) %in% col_to_rem)]
 
-  
+cols_with_na <- c("Name", "Year", "Nationality", "Cause.of.death")
+new_df <- new_df[complete.cases(new_df[, cols_with_na]), ]
+
 #You will then also need to create additional columns in your dataset: 
 #Must create at least one new categorical variable
-new_df$Year_descr <- cut(new_df$Year, breaks = c(2000, 2000, 2020), 
-                         labels = c("Before 2000", "2000-2020"), include.lowest = T) 
-  
+new_df$Year_descr <- cut(as.numeric(new_df$Year), breaks = c(2000, 2020), 
+                         labels = c("Before 2000", "2000-2020"), include.lowest = TRUE)
+ 
 #Must create at least one new continuous/numerical variable
   
   

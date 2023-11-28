087fb303aedc0a6b68d8050bfaa7b5933b8e2984
vm11962-2131012 <92124097+vm11962-2131012@users.noreply.github.com>
2023-11-27 21:16:35 -0800

    Update DataWrangling.R

--- DataWrangling.R
+++ DataWrangling.R
@@ -3,18 +3,19 @@ library(stringr)
 
 annapurna_df <- read.csv("annapurna.csv") 
 accidents_df <- read.csv("accidents_git.csv") 
-members_df <- read.csv("members.csv") 
+#members_df <- read.csv("members.csv") 
 
-#You first need to create a unified dataset 
-#(i.e. you need to join your datasets together). 
-#This means that the records in your two datasets 
-#need to be related somehow, either by a shared key 
-#or a combination of fields.
-new_df <- merge(accidents_df, members_df, 
-                by.x = "Publication.Year", by.y = "year", all = T)
+#You fist need to to create a unified dataset. This means that the records in your 
+#two datasets need to be related some how, either by a shared key or a combination of fields.
+annapurna_df$Year <- substr(annapurna_df$Date, nchar(annapurna_df$Date) -3, nchar(annapurna_df$Date))  
+
+new_df <- merge(annapurna_df, accidents_df, 
+                by.x = "Year", by.y = "Publication.Year", all = T)
+
+df_unique <- new_df[!duplicated(new_df[c("Accident.Title")]), ]
+
   
-  
-#You will then also need to create additional columns in your dataset:   
+#You will then also need to create additional columns in your dataset: 
 #Must create at least one new categorical variable
 new_df$Year_descr <- cut(new_df$Year, breaks = c(2000, 2000, 2020), 
                          labels = c("Before 2000", "2000-2020"), include.lowest = T) 
@@ -23,7 +24,5 @@ new_df$Year_descr <- cut(new_df$Year, breaks = c(2000, 2000, 2020),
   
   
 #Must create at least one summarization data frame 
-
-
-#Note - your summarization table does not need o be exported to a csv file, 
+#Note - your summarization table does not need to be exported to a csv file, 
 #you just need to have code that create this data frame. 

95c2ada1f2cece025d74dd9f7ab7553536800e8d
vm11962-2131012 <92124097+vm11962-2131012@users.noreply.github.com>
2023-11-27 20:06:30 -0800

    Update DataWrangling.R

--- DataWrangling.R
+++ DataWrangling.R
@@ -5,19 +5,19 @@ annapurna_df <- read.csv("annapurna.csv")
 accidents_df <- read.csv("accidents_git.csv") 
 members_df <- read.csv("members.csv") 
 
-#You fist need to to create a unified dataset 
+#You first need to create a unified dataset 
 #(i.e. you need to join your datasets together). 
 #This means that the records in your two datasets 
-#need to be related some how, either by a shared key 
+#need to be related somehow, either by a shared key 
 #or a combination of fields.
 new_df <- merge(accidents_df, members_df, 
                 by.x = "Publication.Year", by.y = "year", all = T)
   
   
-#You will then also need to create additional columns in your dataset: 
-  
+#You will then also need to create additional columns in your dataset:   
 #Must create at least one new categorical variable
-filter_df <- new_df[!is.na(df$), ] 
+new_df$Year_descr <- cut(new_df$Year, breaks = c(2000, 2000, 2020), 
+                         labels = c("Before 2000", "2000-2020"), include.lowest = T) 
   
 #Must create at least one new continuous/numerical variable
   

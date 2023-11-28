166b7539aa7c5faf7620e5e231a7653d71c3c7b7
vm11962-2131012 <92124097+vm11962-2131012@users.noreply.github.com>
2023-11-27 23:10:09 -0800

    Update DataWrangling.R

--- DataWrangling.R
+++ DataWrangling.R
@@ -2,17 +2,22 @@ library(dplyr)
 library(stringr)
 
 annapurna_df <- read.csv("annapurna.csv") 
-accidents_df <- read.csv("accidents_git.csv") 
+#accidents_df <- read.csv("accidents_git.csv") 
+mount_e_df <- read.csv("mount_everest_deaths.csv")
 #members_df <- read.csv("members.csv") 
 
 #You fist need to to create a unified dataset. This means that the records in your 
 #two datasets need to be related some how, either by a shared key or a combination of fields.
 annapurna_df$Year <- substr(annapurna_df$Date, nchar(annapurna_df$Date) -3, nchar(annapurna_df$Date))  
+mount_e_df$Year <- substr(mount_e_df$Date, nchar(mount_e_df$Date) -4, nchar(mount_e_df$Date))  
 
-new_df <- merge(annapurna_df, accidents_df, 
-                by.x = "Year", by.y = "Publication.Year", all = T)
+new_df <- merge(annapurna_df, mount_e_df, 
+                by.x = "Date", by.y = "Date", all = T)
 
-new_df <- new_df[!duplicated(new_df[c("Accident.Title")]), ]
+new_df$Name <- paste(new_df$Name.x, new_df$Name.y, sep = " ")
+new_df <- new_df[, -which(names(new_df) == "Name.x", "Name.y")]
+
+#new_df <- new_df[!duplicated(new_df[c("Accident.Title")]), ]
 
   
 #You will then also need to create additional columns in your dataset: 

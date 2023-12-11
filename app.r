50623ec9224c66dd450fb6de8b06fe4f3526f20b
luktak64 <149434901+luktak64@users.noreply.github.com>
2023-12-10 20:54:42 -0800

    Update app.r

--- app.r
+++ app.r
@@ -1,9 +1,48 @@
+# Victoria Martinez
+# Luke Takechi
+
 library(dplyr)
 library(stringr)
 library(ggplot2)
 library(shiny)
 library(plotly)
 
+# Read datasets
+annapurna_df <- read.csv("annapurna.csv") 
+mount_e_df <- read.csv("mount_everest_deaths.csv")
+
+# You first need to create a unified dataset. 
+# This means that the records in your two datasets need to be related somehow, either by a shared key or a combination of fields.
+annapurna_df$Year <- substr(annapurna_df$Date, nchar(annapurna_df$Date) - 3, nchar(annapurna_df$Date))  
+mount_e_df$Year <- substr(mount_e_df$Date, nchar(mount_e_df$Date) - 4, nchar(mount_e_df$Date))  
+
+new_df <- merge(annapurna_df, mount_e_df, by.x = "Date", by.y = "Date", all = TRUE)
+
+col_to_con <- c("Name", "Year", "Nationality", "Cause.of.death")
+
+for (col in col_to_con) {
+  new_df[[col]] <- paste(new_df[[paste0(col, ".x")]], new_df[[paste0(col, ".y")]], sep = " ")
+}
+
+col_to_rem <- c("Date", "Name.x", "Name.y", "Year.x", "Year.y", "Nationality.x", "Nationality.y", "Cause.of.death.x", "Cause.of.death.y")
+new_df <- new_df[, -which(names(new_df) %in% col_to_rem)]
+
+col_to_clean <- c("Name", "Year", "Nationality", "Cause.of.death")
+new_df[col_to_clean] <- lapply(new_df[col_to_clean], function(x) gsub("NA", "", as.character(x)))
+
+# You will then also need to create additional columns in your dataset: 
+# Must create at least one new categorical variable
+new_df$Year <- as.numeric(gsub("[^0-9]", "", new_df$Year))
+new_df$Year_descr <- cut(new_df$Year, breaks = c(-Inf, 2000, 2020, Inf),
+                         labels = c("Before 2000", "2000-2020", "After 2020"), include.lowest = TRUE)
+
+# Must create at least one new continuous/numerical variable
+new_df$Num_Deaths_In_Yr <- ave(rep(1, nrow(new_df)), new_df$Year, FUN = sum)
+
+# Must create at least one summarization data frame 
+# Note - your summarization table does not need to be exported to a csv file, 
+# you just need to have code that creates this data frame.
+summary_df <- summarise(group_by(new_df, Year_descr), Total_Deaths = sum(Num_Deaths_In_Yr, na.rm = TRUE))
 
 # Function to create a bullet list
 spot_list <- function(char, ordered = FALSE) {
@@ -75,13 +114,13 @@ ui <- fluidPage(
     ),
     tabPanel("Compare years",
              h3("Compare Deaths Between Two Years"),
-             selectInput("year1", "Select Year 1:", choices = unique(new_df$Year), selected = min(unique(new_df$Year))),
-             selectInput("year2", "Select Year 2:", choices = unique(new_df$Year), selected = max(unique(new_df$Year))),
+             selectInput("year1", "Select Year 1:", choices = unique(new_df$Year[new_df$Year >= 1000]), selected = min(unique(new_df$Year[new_df$Year >= 1000]))),
+             selectInput("year2", "Select Year 2:", choices = unique(new_df$Year[new_df$Year >= 1000]), selected = max(unique(new_df$Year[new_df$Year >= 1000]))),
              plotOutput("compare_plot")
     ),
     tabPanel("Nationalities of People who died",
              h3("Select a Year"),
-             selectInput("selected_year_pie", "Select Year:", choices = unique(new_df$Year), selected = min(unique(new_df$Year)),
+             selectInput("selected_year_pie", "Select Year:", choices = unique(new_df$Year[new_df$Year >= 1000]), selected = min(unique(new_df$Year[new_df$Year >= 1000])),
                          multiple = FALSE),
              plotOutput("nationality_pie_chart")
     ),
@@ -99,7 +138,7 @@ server <- function(input, output) {
   
   output$plot <- renderPlot({
     selected_year <- input$Year
-    filtered_data <- subset(new_df, Year == selected_year)
+    filtered_data <- subset(new_df, grepl("^\\d{4}$", Year) & Year == selected_year)
     
     data_order <- filtered_data[order(-filtered_data$Num_Deaths_In_Yr), ]
     same_rows <- data_order[!duplicated(data_order$Cause.of.death), ]
@@ -126,7 +165,8 @@ server <- function(input, output) {
   output$compare_plot <- renderPlot({
     selected_year1 <- input$year1
     selected_year2 <- input$year2
-    filtered_data <- subset(new_df, Year %in% c(selected_year1, selected_year2))
+    # Exclude the "75" entries explicitly
+    filtered_data <- subset(new_df, grepl("^\\d{4}$", Year) & Year %in% c(selected_year1, selected_year2) & Year != "75")
     ggplot(filtered_data, aes(x = Num_Deaths_In_Yr, y = Cause.of.death, fill = factor(Year))) +
       geom_bar(stat = "identity", position = "dodge") +
       labs(title = "Comparison of Deaths Between Two Years",
@@ -138,7 +178,8 @@ server <- function(input, output) {
   output$nationality_pie_chart <- renderPlot({
     selected_year_pie <- input$selected_year_pie
     
-    filtered_data <- subset(new_df, Year == selected_year_pie)
+    filtered_data <- subset(new_df, grepl("^\\d{4}$", Year) & Year == selected_year_pie & Year != "75")
+    
     nationality_counts <- table(filtered_data$Nationality)
     ordered_counts <- nationality_counts[order(-nationality_counts)]
     top_n <- 10
